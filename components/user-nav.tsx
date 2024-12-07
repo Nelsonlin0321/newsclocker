77a42a8728394a965ff9f0ffed5f77dd4ded684f
Nelsonlin0321 <nelsonlin0321@outlook.com>
2024-12-07 22:43:16 +0800

    update user nav

--- components/user-nav.tsx
+++ components/user-nav.tsx
@@ -1,51 +1,24 @@
-import {
-    DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuItem,
-    DropdownMenuLabel,
-    DropdownMenuSeparator,
-    DropdownMenuTrigger,
-  } from '@/components/ui/dropdown-menu';
-  import { Button } from '@/components/ui/button';
-  import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
-  import Link from 'next/link';
+"use client";
+import { UserButton } from '@clerk/nextjs';
+import { MonitorCog  } from 'lucide-react';
+import { useRouter } from 'next/navigation';
   
-  export default function UserNav() {
-    return (
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <Button variant="ghost" className="relative h-8 w-8 rounded-full">
-            <Avatar className="h-8 w-8">
-              <AvatarImage src="/avatars/01.png" alt="User avatar" />
-              <AvatarFallback>U</AvatarFallback>
-            </Avatar>
-          </Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent className="w-56" align="end" forceMount>
-          <DropdownMenuLabel className="font-normal">
-            <div className="flex flex-col space-y-1">
-              <p className="text-sm font-medium leading-none">Username</p>
-              <p className="text-xs leading-none text-muted-foreground">
-                user@example.com
-              </p>
-            </div>
-          </DropdownMenuLabel>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem asChild>
-            <Link href="/workspace">
-              Workspace
-            </Link>
-          </DropdownMenuItem>
-          <DropdownMenuItem asChild>
-            <Link href="/settings">
-              Settings
-            </Link>
-          </DropdownMenuItem>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem>
-            Log out
-          </DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
-    );
-  }
+export default function UserNav() {
+
+  const router = useRouter()
+  return (
+    <UserButton>
+        <UserButton.MenuItems>
+          <UserButton.Action
+            label="Workspace"
+            labelIcon={<MonitorCog className='w-4 h-4' />}
+            onClick={() => router.push("/workspace")}
+          />
+      </UserButton.MenuItems>
+      <UserButton.MenuItems>
+          <UserButton.Action label="signOut" />
+          <UserButton.Action label="manageAccount" />
+        </UserButton.MenuItems>
+      </UserButton>
+  );
+}

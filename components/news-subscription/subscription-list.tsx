566d94f9f67fc90152aaaba36e86a44629d5c50e
Nelsonlin0321 <nelsonlin0321@outlook.com>
2024-12-14 00:05:26 +0800

    update

--- components/news-subscription/subscription-list.tsx
+++ components/news-subscription/subscription-list.tsx
@@ -1,13 +1,11 @@
-import { SubscriptionCard } from "./subscription-card";
 import { NewsSubscription } from "@prisma/client";
+import { SubscriptionCard } from "./subscription-card";
 
- 
 interface Props {
-  newsSubscriptions: NewsSubscription[]
+  newsSubscriptions: NewsSubscription[];
 }
 
-
-export function SubscriptionList({newsSubscriptions}:Props) {
+export function SubscriptionList({ newsSubscriptions }: Props) {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
       {newsSubscriptions.map((subscription) => (

838357aef7cd28d244f05260b062543beffa5373
Nelsonlin0321 <nelsonlin0321@outlook.com>
2025-01-04 01:09:48 +0800

    finished webhook

--- app/api/webook/route.ts
+++ app/api/webook/route.ts
@@ -7,144 +7,203 @@ import { render } from "@react-email/components";
 import { headers } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 
+const ses = new SES({ region: "us-east-1" });
+
 export async function POST(req: NextRequest) {
-  const body = await req.text();
+  try {
+    const body = await req.text();
+    const signature = headers().get("Stripe-Signature") as string;
+    const webhookSecret: string = process.env.STRIPE_WEBHOOK_SECRET as string;
 
-  const signature = headers().get("Stripe-Signature") as string;
-  const webhookSecret: string = process.env.STRIPE_WEBHOOK_SECRET as string;
 
-  const event = stripe.webhooks.constructEvent(body, signature, webhookSecret);
 
-  const stripEvent = await prisma.stripeEvent.findUnique({
-    where: { eventId: event.id },
-  });
+    const event = stripe.webhooks.constructEvent(
+      body,
+      signature,
+      webhookSecret
+    );
 
-  if (stripEvent) {
-    const eventJson = JSON.stringify(stripEvent.eventObject);
-    console.log(`INFO: Event ${eventJson} has been proceeded before`);
+    const existedStripeEvent = await prisma.stripeEvent.findUnique({
+      where: { eventId: event.id },
+    });
 
-    return new NextResponse("OK", { status: 200 });
-  }
+    if (existedStripeEvent) {
+      const eventJson = JSON.stringify(existedStripeEvent.eventObject);
+      console.log(`INFO: Event ${eventJson} has been proceeded before`);
 
-  if (event.type === "checkout.session.completed") {
-    try {
-      const session = event.data.object;
-      const priceInCents = session.amount_total;
-      const email = session.customer_email;
-      const { userId, period } = session.metadata!;
-      console.log("INFO: session meta data: ", session.metadata);
+      return new NextResponse("OK", { status: 200 });
+    }
 
-      if (!userId) {
-        console.error("No userId");
-        return new NextResponse("No userId", { status: 400 });
-      }
+    const eventType = event.type;
+    const data = event.data;
+    switch (eventType) {
+      case "checkout.session.completed": {
+        const session = event.data.object;
+        const priceInCents = session.amount_total;
+        const email = session.customer_email;
+        const customerId = session.customer?.toString();
+        const { userId, period } = session.metadata!;
+        console.log("INFO: session meta data: ", session.metadata);
 
-      if (!period) {
-        console.error("Not period");
-        return new NextResponse("No period", { status: 400 });
-      }
+        if (!customerId) {
+          console.error("No customerId");
+          return new NextResponse("No customerId", { status: 400 });
+        }
 
-      if (!periodOptions.includes(period)) {
-        console.error("Invalid period");
-        return new NextResponse("Invalid period", { status: 400 });
-      }
-      const subscribedPeriod = period as SubscribedPeriod;
+        if (!userId) {
+          console.error("No userId");
+          return new NextResponse("No userId", { status: 400 });
+        }
 
-      if (!email) {
-        console.error("Customer email is required");
-        return new NextResponse("Customer email is required", {
-          status: 400,
-        });
-      }
+        if (!period) {
+          console.error("Not period");
+          return new NextResponse("No period", { status: 400 });
+        }
 
-      const userSubscription = await prisma.userSubscription.findFirst({
-        where: { userId},
-      });
+        if (!periodOptions.includes(period)) {
+          console.error("Invalid period");
+          return new NextResponse("Invalid period", { status: 400 });
+        }
+        const subscribedPeriod = period as SubscribedPeriod;
 
-      const days = periodToDays[subscribedPeriod];
-
-      const currentDateUTC = new Date();
-      let PeriodEnd = new Date(
-        currentDateUTC.getTime() + days * 24 * 60 * 60 * 1000
-      );
-
-      if (userSubscription) {
-        const stripeCurrentPeriodEnd = userSubscription.stripeCurrentPeriodEnd;
-        if (stripeCurrentPeriodEnd < currentDateUTC) {
-          // already expired
-          // replace the period end
-          await prisma.userSubscription.update({
-            where: { id: userSubscription.id },
-            data: {
-              priceInCents: priceInCents as number,
-              email: email,
-              active: true,
-              stripeCurrentPeriodEnd: PeriodEnd,
-              subscribedPeriod: subscribedPeriod,
-            },
+        if (!email) {
+          console.error("Customer email is required");
+          return new NextResponse("Customer email is required", {
+            status: 400,
           });
+        }
+
+        const userSubscription = await prisma.userSubscription.findFirst({
+          where: { userId },
+        });
+
+        const days = periodToDays[subscribedPeriod];
+
+        const currentDateUTC = new Date();
+        let PeriodEnd = new Date(
+          currentDateUTC.getTime() + days * 24 * 60 * 60 * 1000
+        );
+
+        if (userSubscription) {
+          const stripeCurrentPeriodEnd =
+            userSubscription.stripeCurrentPeriodEnd;
+          if (stripeCurrentPeriodEnd < currentDateUTC) {
+            // already expired
+            // replace the period end
+            await prisma.userSubscription.update({
+              where: { id: userSubscription.id },
+              data: {
+                customerId:customerId as string ,
+                priceInCents: priceInCents as number,
+                email: email,
+                active: true,
+                stripeCurrentPeriodEnd: PeriodEnd,
+                subscribedPeriod: subscribedPeriod,
+              },
+            });
+          } else {
+            // extend the period end
+            PeriodEnd = new Date(
+              stripeCurrentPeriodEnd.getTime() + days * 24 * 60 * 60 * 1000
+            );
+            await prisma.userSubscription.update({
+              where: { id: userSubscription.id },
+              data: {
+                customerId: customerId as string ,
+                priceInCents: priceInCents as number,
+                email: email,
+                active: true,
+                stripeCurrentPeriodEnd: PeriodEnd,
+                subscribedPeriod: subscribedPeriod,
+              },
+            });
+          }
         } else {
-          // extend the period end
-          PeriodEnd = new Date(
-            stripeCurrentPeriodEnd.getTime() + days * 24 * 60 * 60 * 1000
-          );
-          await prisma.userSubscription.update({
-            where: { id: userSubscription.id },
+          await prisma.userSubscription.create({
             data: {
-              priceInCents: priceInCents as number,
-              email: email,
+              customerId:customerId as string ,
+              userId,
+              email,
               active: true,
-              stripeCurrentPeriodEnd: PeriodEnd,
+              priceInCents: priceInCents as number,
               subscribedPeriod: subscribedPeriod,
+              stripeCurrentPeriodEnd: PeriodEnd,
             },
           });
         }
-      } else {
-        await prisma.userSubscription.create({
-          data: {
-            userId,
-            email,
-            active: true,
-            priceInCents: priceInCents as number,
-            subscribedPeriod: subscribedPeriod,
-            stripeCurrentPeriodEnd: PeriodEnd,
+
+        // store stripe event
+        const stripeEvent = event as unknown as Prisma.JsonObject;
+        await prisma.stripeEvent.create({
+          data: { eventId: event.id, eventObject: stripeEvent },
+        });
+
+        await ses.sendEmail({
+          Source: "NewsClocker <noreply@newsclocker.com>",
+          Destination: {
+            ToAddresses: [email],
+          },
+          Message: {
+            Subject: {
+              Data: "Thank you for subscribing",
+            },
+            Body: {
+              Html: {
+                Data: await render(WelcomeEmail({ userFirstName: "there" })),
+              },
+            },
           },
         });
+
+        console.log(`INFO: Proceed with the ${event.type} event successfully `);
+        return new NextResponse("OK", { status: 200 });
       }
+      case "customer.subscription.deleted": {
+        const subscription = event.data.object;
+        const customerId = subscription.customer.toString();
 
-      // store stripe event
-      const stripeEvent = event as unknown as Prisma.JsonObject;
-      await prisma.stripeEvent.create({
-        data: { eventId: event.id, eventObject: stripeEvent },
-      });
+        const userSubscription = await prisma.userSubscription.findFirst({
+          where: { customerId },
+        });
 
-      const ses = new SES({ region: 'us-east-1' });
-
-      await ses.sendEmail({
-        Source: "NewsClocker <noreply@newsclocker.com>",
-        Destination: {
-          ToAddresses: [email],
-        },
-        Message: {
-          Subject: {
-            Data: "Thank you for subscribing",
-          },
-          Body: {
-            Html: {
-              Data:await render(WelcomeEmail({userFirstName: "there"})),
-            }
-          }
-        },
-      });
-
-      console.log(`INFO: Proceed with the ${event.type} event successfully `);
-      return new NextResponse("OK", { status: 200 });
-    } catch (error) {
-      console.error(error);
-      return new NextResponse("Unexpected Error", { status: 500 });
+        if (userSubscription) {
+          await prisma.userSubscription.update({
+            where: { id: userSubscription.id },
+            data: {
+              active: false,
+              stripeCurrentPeriodEnd:new Date()
+            },
+          });
+          console.log(`INFO: Proceed with the ${event.type} event successfully `);
+          return new NextResponse("OK", { status: 200 });
+        } else {
+          console.error(`No subscription found for customer ${customerId}`);
+          return new NextResponse("No subscription found", { status: 400 });
+        }
+      }
+      default: {
+        console.log(`INFO: The ${eventType} event not captured`);
+        return new NextResponse("OK", { status: 200 });
+      }
     }
-  } else {
-    console.log(`INFO: The ${event.type} event not captured`);
-    return new NextResponse("OK", { status: 200 });
+  } catch (error) {
+    await ses.sendEmail({
+      Source: "NewsClocker <noreply@newsclocker.com>",
+      Destination: {
+        ToAddresses: ["contact@newsclocker.com"],
+      },
+      Message: {
+        Subject: {
+          Data: `Unexpected Error Occurred in handling stripe event ${JSON.stringify(event)}`,
+        },
+        Body: {
+          Html: {
+            Data: String(error),
+          },
+        },
+      },
+    });
+    console.error(error);
+    return new NextResponse("Unexpected Error", { status: 500 });
   }
 }

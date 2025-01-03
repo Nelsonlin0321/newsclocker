66bee71550be8af2db27f701f95eb0dfd668f3ca
Nelsonlin0321 <nelsonlin0321@outlook.com>
2025-01-03 22:33:49 +0800

    add subscription url server action

---
+++ app/actions/stripe/get-subscription-url.ts
@@ -0,0 +1,66 @@
+"use server";
+
+import { PeriodToDisplayFrequency, PeriodToPriceInCents, stripe } from "@/lib/payment";
+import { auth, currentUser } from "@clerk/nextjs/server";
+import { SubscribedPeriod } from "@prisma/client";
+
+export async function getSubscriptionUrl(
+    period: SubscribedPeriod,
+    nextUrl: string
+  ) {
+    "use server";
+    try {
+      const { userId } = await auth();
+      const user = await currentUser();
+      if (!userId) {
+        return {
+          error: "Login is required",
+        };
+      }
+  
+      if (!user?.emailAddresses) {
+        return {
+          error: "Email address is required",
+        };
+      }
+      
+      const userEmail = user.emailAddresses[0].emailAddress;
+      const return_url = process.env.NEXT_BASE_URL + nextUrl;
+      
+      const displayPeriod = PeriodToDisplayFrequency[period];
+
+      const priceInCent = PeriodToPriceInCents[period];
+
+      const stripeSession = await stripe.checkout.sessions.create({
+        client_reference_id: userId,
+        success_url: return_url + "?subscription=success",
+        cancel_url: return_url,
+        payment_method_types: ["card"],
+        mode: "subscription",
+        billing_address_collection: "auto",
+        customer_email: userEmail,
+        line_items: [
+          {
+            price_data: {
+              currency: "USD",
+              product_data: {
+                name: `NewsClocker Pro ${displayPeriod} Subscription`,
+                images: ["https://d2gewc5xha837s.cloudfront.net/newsclocker/logo/logo-horizontal.png"],
+              },
+              unit_amount: priceInCent,
+              recurring: { interval:period},
+            },
+            quantity: 1,
+          },
+        ],
+        metadata: { userId, period }
+      });
+
+      return { url: stripeSession.url };
+    } catch (error) {
+      console.error(error);
+      return {
+        error: "Unexpected error",
+      };
+    }
+  }
---
+++ lib/payment.ts
@@ -0,0 +1,27 @@
+import { SubscribedPeriod } from "@prisma/client";
+import Stripe from "stripe";
+
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+  apiVersion: "2024-12-18.acacia",
+  typescript: true,
+});
+
+export const PeriodToPriceInCents: Record<SubscribedPeriod, number> = {
+  month: 490,
+  year: 3900,
+};
+
+export const periodToDays: Record<SubscribedPeriod, number> = {
+  month: 31,
+  year: 365,
+};
+
+export const periodOptions = [
+  "month",
+  "year",
+];
+
+export const PeriodToDisplayFrequency: Record<SubscribedPeriod, string> = {
+    month: "Monthly",
+    year: "Yearly",
+  };

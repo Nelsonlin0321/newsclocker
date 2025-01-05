bfec5ae69722ec6be1fe6e8db6155f4ea386424f
Nelsonlin0321 <nelsonlin0321@outlook.com>
2025-01-05 18:16:48 +0800

    set correct publishable key

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
     <ClerkProvider
       publishableKey={
         process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY ||
-        "pk_test_51QcObZI73EAAZJzWH9gGY8iN6OjFeTDo2S4Q41PgDCrtc0qdAIP9AWZN6YVEOdkqSyNdDdvBIhS1j0RZ3TkCnszX00OnkY0cht"
+        "pk_test_YWR2YW5jZWQtaGFyZS01My5jbGVyay5hY2NvdW50cy5kZXYk"
       }
     >
       <html lang="en">
